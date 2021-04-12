99199f0725ca2ad0422538d84435a1e59a67b2fa
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-12 18:09:19 +0300

    добавлен iconUrl для обьекта SnippetItem

--- src/store/reducers/searchReducer.ts
+++ src/store/reducers/searchReducer.ts
@@ -1,19 +1,27 @@
 import { SnippetItem, SearchAction, SEARCH_ACTION } from "../../types/search";
 
+/*
+    редюсер срабатывающей после загрузки результатов поиска книг
+    возвращает состояние списка  книг
+ */
 const InitialState: SnippetItem[] = [];
-
 export const searchReducer = (state: SnippetItem[] = InitialState, action: SearchAction): SnippetItem[] => {
 
   if (action.type === SEARCH_ACTION) {
-    const docs = action.payload.docs
-    let tmp: SnippetItem[] = [];
-    for (let i = 0; i < Math.min(100, docs.length); i++) {
-      let author = (docs[i].author_name)?docs[i].author_name[0]: "";
+    const docs = action.payload.docs //результаты поиска хранятся в массиве json.docs (json - ответ с сервера)
+    let tmp: SnippetItem[] = []; 
+    for (let i = 0; i < Math.min(10, docs.length); i++) {
+
+      //ответ с сервера не всегда корректен и не возвращает одни и те же данные для каких то книг отстутсвует какой тоиз параметров, 
+      //при отстуствии присваевается пустая строка
+
+      let author = (docs[i].author_name) ? docs[i].author_name[0] : "";
       let title = (docs[i].title)?docs[i].title:"";
       let publishDate = (docs[i].publish_date) ? docs[i].publish_date[0] : (docs[i].publish_year) ? docs[i].publish_year[0]:"";
-      let isbn = (docs[i].isbn)?docs[i].isbn[0]:"";
+      let isbn = (docs[i].isbn) ? docs[i].isbn[0] : "";
+      let iconUrl = "http://covers.openlibrary.org/b/isbn/"+isbn+"-S.jpg"
       tmp.push({
-        author,title,publishDate,isbn
+        author,title,publishDate,isbn,iconUrl
       });
     }
     return tmp;
