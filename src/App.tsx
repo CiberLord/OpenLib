5c22d191062b82973a7fcdd18f9690a72599c865
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-12 10:12:16 +0300

    сборка вебпак

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+  
+  return (
+    <div>
+      hello word
+    </div>
+  )
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env,argv) => {
+    const modeEnv = argv.mode||'development';
+
+    return {
+        mode: modeEnv,
+        entry: ["@babel/polyfill", "./src/index.tsx"],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: "[name].[hash].js",
+
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"]
+        },
+        devServer: {
+            port: 3000
+        },
+        plugins: [
+            new HtmlWebpackPlugin({ template: "./public/index.html" }),
+            new CleanWebpackPlugin()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.(css)$/,
+                    use: ["style-loader", "css-loader"],
+                },
+                {
+                    test: /\.(jpg|jpeg|png|svg)/,
+                    use: ["file-loader"]
+                },
+                {
+                    test: /\.ts(x?)$/,
+                    exclude: /node_modules/,
+                    use: { loader: "ts-loader" }
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    }
+                }
+            ]
+        }
+    }
+
+}
