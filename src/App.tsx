772d9971d8e20a16460554b79387396a9857cd5b
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-13 02:59:01 +0300

    добавил компонент ModalLayout после всех дочерних компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import ModalLayout from './components/ModalLayout';
 import Searchbar from './components/SearchBar';
 import SnippetList from './components/SnippetList';
+import { useTypedSelector } from './hooks/useTypedSelector';
 
 
 // контейнер приложения 
 const App: React.FC = () => {
+
+  const isModal = useTypedSelector(state => state.isModal);
   return (
     <div>
       <Searchbar />
       <SnippetList />
+      {isModal&&<ModalLayout author="author" title="title" date="date" isbn="2323" iconUrl="icon"/>}
     </div>
   )
 }
