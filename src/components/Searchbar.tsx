3191721d8617cff58783da99b0bec1424b06cfba
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-12 18:07:16 +0300

    добавлена работа таймера, инициация поиска через одну секунду после ввода символа

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,23 +1,37 @@
-import React, { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import * as React from 'react';
 import { useAction } from '../hooks/useActions';
-import { useTypedSelector } from '../hooks/useTypedSelector';
 
 
+let timerid: any | number = null;
+
+/*
+  компонент поисковой строки
+*/
 const Searchbar: React.FC = () => {
-  
-  // const dispatch = useDispatch();
-  const snippets = useTypedSelector(state => state.searchResults);
   const { fetchSearch } = useAction();
 
-  useEffect(() => {
-    fetchSearch("hello world","q");
-  }, [])
-  
-  console.log(snippets);
+  // событие которое сработывате при каждом вводе символа в поисковую строку, просихдит сброс таймера поиска, запускается новый таймер который иницирует поиск чрез 1 секунду
+  const changeHandler = (event: React.ChangeEvent) => {
+    let target = event.target as HTMLInputElement;
+    
+    clearTimeout(timerid);
+    //инициация новой загрузки через 1 секунду
+    timerid = setTimeout(() => {
+      fetchSearch(target.value);
+    },900)
+  }
+  const searchHandler = (event: any) => {
+
+    setTimeout(() => {
+      console.log("hello");
+    }, 500);
+  }
   return (
-    <div>
-      
+    <div className="search-bar">
+      <div>
+        <input type="text" onChange={changeHandler} />
+        <button className="search-button" onClick={searchHandler}>search</button>
+      </div>
     </div>
   )
 }
