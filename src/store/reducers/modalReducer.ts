f20b7aa522a58c3a96bc9be656b4cb80e21e682f
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-13 10:22:22 +0300

    работа модального окна

---
+++ src/store/reducers/modalReducer.ts
@@ -0,0 +1,13 @@
+import { ModalAction, ModalState, MODAL_VISIBLE, SNIPPET_KEY} from "../../types/modallayout";
+
+
+let initialState: ModalState = { isVisble: false, snippetKey: 0};
+export const modalReducer = (state:ModalState= initialState, action:ModalAction):ModalState => {
+  if (action.type === MODAL_VISIBLE) {
+    return { ...state, isVisble: action.payload.isVisible };
+  }
+  if (action.type === SNIPPET_KEY) {
+    return {...state,snippetKey: action.payload.snippetKey}
+  }
+  return state;
+}
