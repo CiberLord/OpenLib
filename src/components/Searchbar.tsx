4a77b21807640c2c5c7ca762dda5295182d4eb15
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-13 16:57:37 +0300

    добавлен компонент фильтра и работа с выборам фильтра

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,18 +1,23 @@
 import * as React from 'react';
 import { useAction } from '../hooks/useActions';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import '../styles/searchbar.css';
+import { filters } from '../types/filter';
+import { Filterbutton } from './Filterbutton';
+
 
 let timerid: any | number = null;
-
 /*
   компонент поисковой строки
 */
 const Searchbar: React.FC = () => {
-  const { fetchSearch } = useAction();
-
+  const { fetchSearch,setFilter } = useAction();
+  const filter = useTypedSelector(state => state.filter);
   React.useEffect(() => {
+    // стартовый показ книг наподобоии рекомендаций
     fetchSearch("java");
-  },[])
+  }, [])
+  
   // событие которое сработывате при каждом вводе символа в поисковую строку, просихдит сброс таймера поиска, запускается новый таймер который иницирует поиск чрез 1 секунду
   const changeHandler = (event: React.ChangeEvent) => {
     let target = event.target as HTMLInputElement;
@@ -20,18 +25,17 @@ const Searchbar: React.FC = () => {
     clearTimeout(timerid);
     //инициация новой загрузки через 1 секунду
     timerid = setTimeout(() => {
-      fetchSearch(target.value);
+      if(target.value!=="") fetchSearch(target.value,filter);
     }, 900)
   }
 
   const searchHandler = (event: any) => {
-
-    setTimeout(() => {
-      console.log("hello");
-    }, 500);
+    clearTimeout(timerid);
+    fetchSearch("hello kitty",filter);
   }
   return (
     <div className="search-bar">
+      <Filterbutton filters={filters} setFilter={ setFilter}/>
       <input type="text" onChange={changeHandler} className="search-bar__input" />
       <button className="search-bar__button" onClick={searchHandler}></button>
     </div>
