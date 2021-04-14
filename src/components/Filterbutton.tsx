4aa3efa7ba5ef881dfbe99adc09731f9e1a98b1e
Yuldash Atajanov <cybergod1123@gmail.com>
2021-04-14 15:46:37 +0300

    добавил анимации появления  компонента фильтров

--- src/components/Filterbutton.tsx
+++ src/components/Filterbutton.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { CSSTransition } from 'react-transition-group';
 
 export interface FProps {
   filters: string[];
@@ -10,6 +10,14 @@ export const Filterbutton: React.FC<FProps> = (props: FProps) => {
 
   const [filterVisible, setFilterVisible] = useState(false);
   const [filterText, setFiltertext] = useState(props.filters[0]);
+
+  const items = (
+    <ul className="filter-list">
+      {
+        props.filters.map((value, index) =>
+          <li className="filter-item" key={index} onClick={() => switchFilter(value)}>{value}</li>)
+      }
+    </ul>);
   
   const switchFilter = (filter: string) => {
     setFiltertext(filter);
@@ -22,14 +30,14 @@ export const Filterbutton: React.FC<FProps> = (props: FProps) => {
   return (
     <button className="filter-button" onClick={showFilters}>
       {filterText}
-      {filterVisible &&
-        <ul className="filter-list">
-          {
-            props.filters.map((value, index) =>
-              <li className="filter-item" key={index} onClick={() => switchFilter(value)}>{value}</li>)
-          }
-        </ul>
-      }
+      <CSSTransition
+        in={filterVisible}
+        timeout={200}
+        classNames="f_anim"
+        unmountOnExit
+      >
+        {items}
+        </CSSTransition>
     </button>
   )
 }
